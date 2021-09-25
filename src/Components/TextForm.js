8b573ef76cbc10c906b98cda5ae3353ea98dab33
harshitbhardwaj001 <harshitbhardwaj001@gmail.com>
2021-09-25 18:44:11 +0530

    toggle-2

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -41,8 +41,8 @@ export default function TextForm(props) {
       </div>
       <div className="container my-3">
           <h2>Your Text Summary</h2>
-          <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
-          <p>{0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} minutes read.</p>
+          <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+          <p>{0.008 * text.split(/\s+/).filter((element)=>{return element.length!==0}).length} minutes read.</p>
           <h2>Preview</h2>
           <p>{text}</p>
       </div>
