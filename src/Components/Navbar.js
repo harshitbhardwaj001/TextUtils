24f7f73cce404c38d6234bb8238a5e47f3ddb15c
harshitbhardwaj001 <harshitbhardwaj001@gmail.com>
2021-09-25 13:30:25 +0530

    Fifth Commit

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+// import { Link } from 'react-router-dom';
 
 export default function Navbar(props) {
   return (
@@ -8,6 +9,9 @@ export default function Navbar(props) {
         className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
       >
         <div className="container-fluid">
+          {/* <Link className="navbar-brand" to="/">
+            {props.title}
+          </Link> */}
           <a className="navbar-brand" href="/">
             {props.title}
           </a>
@@ -25,12 +29,18 @@ export default function Navbar(props) {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">
+                {/* <Link className="nav-link active" aria-current="page" to="/">
+                  Home
+                </Link> */}
+                <a className="nav-link active" aria-current="page" href="#">
                   Home
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/">
+                {/* <Link className="nav-link" to="/about">
+                  {props.about}
+                </Link> */}
+                <a className="nav-link" href="#">
                   {props.about}
                 </a>
               </li>
