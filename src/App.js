d217f31751183fa0ba55df93fde72f541a45dad2
Harshit Bhardwaj <harshitbhardwaj001@gmail.com>
2021-09-25 09:08:40 +0530

    Third Commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+// import About from "./Components/About";
 import Navbar from "./Components/Navbar";
 import TextForm from "./Components/TextForm";
 
@@ -9,6 +10,7 @@ function App() {
 
       <div className="container my-3">
         <TextForm heading = "Enter Your Text To Analyze Below"/>
+        {/* <About/> */}
       </div>
     </>
   );
